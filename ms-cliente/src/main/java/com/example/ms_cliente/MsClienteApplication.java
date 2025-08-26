a7a804ad0a9eaa3a40fc7085c52f1cf04750f71f
Toshibby <cirogkmv@gmail.com>
2025-08-26 14:19:56 -0500

    Subiendo proyecto tienda-ropa-2

--- ms-cliente/src/main/java/com/example/ms_cliente/MsClienteApplication.java
+++ ms-cliente/src/main/java/com/example/ms_cliente/MsClienteApplication.java
@@ -2,6 +2,8 @@ package com.example.ms_cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 public class MsClienteApplication {
